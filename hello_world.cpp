1ab9e628c7b0b9ffeb39f3065ebb6c5ea78b2911
Memeladon <bingorussian@mail.ru>
2023-05-29 09:39:00 +0300

    Update comment

--- hello_world.cpp
+++ hello_world.cpp
@@ -6,6 +6,6 @@ int main()
     std::cout << "Enter your name:";
     std::cin >> name;
     std::cout << "Hello world from @" << name << std::endl;
-    // Привет, мир!
+    /* Привет, мир! */
     return 0;
 }
